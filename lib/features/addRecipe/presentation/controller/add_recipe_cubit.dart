c646d1988f8d36a15810fb1edf7d4da0c5dfa516
mustafa-shihab1 <mostafaa.sh2001@gmail.com>
2025-11-09 21:06:07 +0200

    fix bug - retrieve the new added recipe directly

--- lib/features/addRecipe/presentation/controller/add_recipe_cubit.dart
+++ lib/features/addRecipe/presentation/controller/add_recipe_cubit.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import '../../../../config/dependency_injection.dart';
 import '../../../../core/storage/local/database/controller/recipe_database_controller.dart';
 import '../../../../core/storage/local/database/model/recipe.dart';
+import '../../../MyRecipes/presentation/controller/my_recipes_cubit.dart';
 import 'add_recipe_state.dart';
 
 class AddRecipeCubit extends Cubit<AddRecipeState> {
@@ -25,6 +27,8 @@ class AddRecipeCubit extends Cubit<AddRecipeState> {
   Future<void> insertRecipe(Recipe recipe) async {
     try {
       await _recipeDatabaseController.insertRecipeToDb(recipe);
+      instance<MyRecipesCubit>().getSavedRecipe();
+
       print('Recipe inserted successfully!');
       emit(AddRecipeSuccessState());
     } catch (e) {
