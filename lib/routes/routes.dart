585ebec1e7d1938bff3034bf08db289886b1f2d5
mustafa-shihab1 <mostafaa.sh2001@gmail.com>
2025-11-11 17:51:48 +0200

    add route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import '../core/storage/local/database/model/recipe.dart';
 import '../core/widgets/app_start_view.dart';
 import '../features/addRecipe/presentation/view/add_recipe_view.dart';
 import '../features/auth/presentation/view/register_view.dart';
@@ -7,6 +7,7 @@ import '../config/dependency_injection.dart';
 import '../core/resources/strings_manager.dart';
 import '../features/auth/presentation/view/login_view.dart';
 import '../features/main/presentation/view/main_view.dart';
+import '../features/recipeDetails/presentation/view/recipe_details_view.dart';
 import '../features/search/presentation/view/search_view.dart';
 
 class Routes {
@@ -20,10 +21,10 @@ class Routes {
   static const String favoritesView = '/favorites_view';
   static const String profileView = '/profile_view';
   static const String addRecipeView = '/add_recipe_view';
+  static const String recipeDetailsView = '/recipe_details_view';
 }
 
 class RouteGenerator {
-
   static Route<dynamic> getRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.appStartView:
@@ -40,26 +41,25 @@ class RouteGenerator {
       case Routes.searchView:
         initSearch();
         return MaterialPageRoute(builder: (_) => const SearchView());
-    case Routes.addRecipeView:
+      case Routes.addRecipeView:
         initAddRecipe();
         return MaterialPageRoute(builder: (_) => const AddRecipeView());
+      case Routes.recipeDetailsView:
+        initRecipeDetails();
+        final recipe = settings.arguments as Recipe;
+        return MaterialPageRoute(builder: (_) => RecipeDetailsView(recipe: recipe));
+
       default:
         return unDefinedRoute();
     }
   }
 
-
   static Route<dynamic> unDefinedRoute() {
     return MaterialPageRoute(
       builder: (_) => Scaffold(
-        appBar: AppBar(
-          title: Text(StringsManager.noRouteFound),
-        ),
-        body: Center(
-          child: Text(StringsManager.noRouteFound),
-        ),
+        appBar: AppBar(title: Text(StringsManager.noRouteFound)),
+        body: Center(child: Text(StringsManager.noRouteFound)),
       ),
     );
   }
-
 }
