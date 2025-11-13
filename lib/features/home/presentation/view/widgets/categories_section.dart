19044f6c4849d54fb18d57a5d4d16a3fb7016e63
mustafa-shihab1 <mostafaa.sh2001@gmail.com>
2025-11-13 21:11:55 +0200

    add shimmer for loading sates

--- lib/features/home/presentation/view/widgets/categories_section.dart
+++ lib/features/home/presentation/view/widgets/categories_section.dart
@@ -1,24 +1,51 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 
+import '../../../../../routes/routes.dart';
 import '../../controller/home_cubit.dart';
 import '../../controller/home_state.dart';
 
 class CategoriesSection extends StatelessWidget {
-  const CategoriesSection({
-    super.key,
-  });
+  const CategoriesSection({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeCubit, HomeState>(
-      buildWhen: (prev, next) =>
-      prev.categoriesStatus != next.categoriesStatus,
+      buildWhen: (prev, next) => prev.categoriesStatus != next.categoriesStatus,
       builder: (context, state) {
         switch (state.categoriesStatus) {
           case RequestStatus.loading:
-            return const Center(child: CircularProgressIndicator());
+            return Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 6,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 1.3,
+                ),
+                itemBuilder: (context, index) {
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Shimmer.fromColors(
+                      baseColor: Colors.grey.shade300,
+                      highlightColor: Colors.grey.shade100,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+
           case RequestStatus.success:
             final categories = state.categories!;
 
@@ -36,43 +63,50 @@ class CategoriesSection extends StatelessWidget {
                 ),
                 itemBuilder: (context, index) {
                   final category = categories[index];
-                  return ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Stack(
-                      children: [
-                        // Category Image
-                        Positioned.fill(
-                          child: Image.network(
-                            category.strCategoryThumb,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Positioned.fill(
-                          child: Container(
-                            color: Colors.black.withOpacity(0.18),
-                          ),
-                        ),
-                        // Category Name
-                        Positioned(
-                          left: 16,
-                          bottom: 16,
-                          child: Text(
-                            category.strCategory,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 22,
-                              shadows: [
-                                Shadow(
-                                  blurRadius: 4,
-                                  color: Colors.black45,
-                                  offset: Offset(2, 2),
-                                ),
-                              ],
+                  return InkWell(
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      Routes.categoryRecipesView,
+                      arguments: category.strCategory,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Stack(
+                        children: [
+                          // Category Image
+                          Positioned.fill(
+                            child: Image.network(
+                              category.strCategoryThumb,
+                              fit: BoxFit.cover,
                             ),
                           ),
-                        ),
-                      ],
+                          Positioned.fill(
+                            child: Container(
+                              color: Colors.black.withOpacity(0.18),
+                            ),
+                          ),
+                          // Category Name
+                          Positioned(
+                            left: 16,
+                            bottom: 16,
+                            child: Text(
+                              category.strCategory,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                                shadows: [
+                                  Shadow(
+                                    blurRadius: 4,
+                                    color: Colors.black45,
+                                    offset: Offset(2, 2),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
@@ -82,15 +116,12 @@ class CategoriesSection extends StatelessWidget {
             return Center(
               child: Column(
                 children: [
-                  const Icon(Icons.error_outline,
-                      color: Colors.red, size: 60),
+                  const Icon(Icons.error_outline, color: Colors.red, size: 60),
                   const SizedBox(height: 10),
-                  Text(state.errorMessage ??
-                      'Failed to load categories'),
+                  Text(state.errorMessage ?? 'Failed to load categories'),
                   const SizedBox(height: 10),
                   ElevatedButton(
-                    onPressed: () =>
-                        context.read<HomeCubit>().getCategories(),
+                    onPressed: () => context.read<HomeCubit>().getCategories(),
                     child: const Text('Retry'),
                   ),
                 ],
--- lib/features/home/presentation/view/widgets/random_meal_widget.dart
+++ lib/features/home/presentation/view/widgets/random_meal_widget.dart
@@ -1,38 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../../../../core/resources/colors_manager.dart';
 import '../../controller/home_cubit.dart';
 import '../../controller/home_state.dart';
 
 class RandomMealWidget extends StatelessWidget {
-  const RandomMealWidget({
-    super.key,
-  });
+  const RandomMealWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeCubit, HomeState>(
-      buildWhen: (prev, next) =>
-      prev.randomMealStatus != next.randomMealStatus,
+      buildWhen: (prev, next) => prev.randomMealStatus != next.randomMealStatus,
       builder: (context, state) {
         switch (state.randomMealStatus) {
           case RequestStatus.loading:
-            return const Center(child: CircularProgressIndicator());
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              elevation: 8,
+              margin: const EdgeInsets.symmetric(vertical: 14, horizontal: 8),
+              child: Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey.shade300,
+                  highlightColor: Colors.grey.shade100,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
+              ),
+            );
           case RequestStatus.success:
             final meal = state.randomMeal!;
             return Card(
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
+                borderRadius: BorderRadius.circular(20),
+              ),
               elevation: 8,
-              margin: const EdgeInsets.symmetric(
-                  vertical: 14, horizontal: 8),
+              margin: const EdgeInsets.symmetric(vertical: 14, horizontal: 8),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   ClipRRect(
                     borderRadius: const BorderRadius.vertical(
-                        top: Radius.circular(20)),
+                      top: Radius.circular(20),
+                    ),
                     child: Image.network(
                       meal.strMealThumb,
                       height: 200,
@@ -48,8 +70,9 @@ class RandomMealWidget extends StatelessWidget {
                         Text(
                           meal.strMeal,
                           style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
                         ),
                         const SizedBox(height: 8),
                         Row(
@@ -58,8 +81,9 @@ class RandomMealWidget extends StatelessWidget {
                               child: Text(
                                 '${meal.strCategory} | ${meal.strArea}',
                                 style: const TextStyle(
-                                    color: Colors.black45,
-                                    fontSize: 16),
+                                  color: Colors.black45,
+                                  fontSize: 16,
+                                ),
                               ),
                             ),
                             const SizedBox(width: 8),
@@ -67,20 +91,24 @@ class RandomMealWidget extends StatelessWidget {
                               style: ElevatedButton.styleFrom(
                                 backgroundColor: Colors.transparent,
                                 padding: const EdgeInsets.symmetric(
-                                    horizontal: 24, vertical: 12),
+                                  horizontal: 24,
+                                  vertical: 12,
+                                ),
                                 shape: RoundedRectangleBorder(
-                                    borderRadius:
-                                    BorderRadius.circular(12)),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
                                 elevation: 0,
                               ),
                               onPressed: () {
                                 /* TODO: Navigate to recipe details */
                               },
-                              child: Text('View Recipe',
-                                  style: TextStyle(
-                                      color:
-                                      ColorsManager.primaryColor,
-                                      fontSize: 16)),
+                              child: Text(
+                                'View Recipe',
+                                style: TextStyle(
+                                  color: ColorsManager.primaryColor,
+                                  fontSize: 16,
+                                ),
+                              ),
                             ),
                           ],
                         ),
@@ -94,14 +122,12 @@ class RandomMealWidget extends StatelessWidget {
             return Center(
               child: Column(
                 children: [
-                  const Icon(Icons.error_outline,
-                      color: Colors.red, size: 60),
+                  const Icon(Icons.error_outline, color: Colors.red, size: 60),
                   const SizedBox(height: 10),
                   Text(state.errorMessage ?? 'Something went wrong'),
                   const SizedBox(height: 10),
                   ElevatedButton(
-                    onPressed: () =>
-                        context.read<HomeCubit>().getRandomMeal(),
+                    onPressed: () => context.read<HomeCubit>().getRandomMeal(),
                     child: const Text('Retry'),
                   ),
                 ],
